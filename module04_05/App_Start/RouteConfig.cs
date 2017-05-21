d6c6d186d2628d8bb743d2145d0cf7504a6ae0cd
van chung chen <auxo_86@hotmail.com>
2017-05-21 11:51:06 +0800

    關於預設路徑和URL的惡搞 //如果url:寫成下面這一行也可以，但是網址列存取就要改成http://localhost:4020/action/controller url: "{action}/{controller}/{id}", //defaults的語句中，action = xxx和index = ooo位置會決定預設的網址列action和controller的位置 //寫成像下面這樣，預設的網址列就會是http://localhost:4020/index/Home defaults: new { action = "Index", controller = "Home", id = UrlParameter.Optional }

--- module04_05/App_Start/RouteConfig.cs
+++ module04_05/App_Start/RouteConfig.cs
@@ -15,8 +15,17 @@ namespace module04_05
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //正常路徑的寫法是下面這一行
+                //url: "{controller}/{action}/{id}",
+                //正常defaults的寫法是像下面這樣
+                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
+                //如果url:寫成下面這一行也可以，但是網址列存取就要改成http://localhost:4020/action/controller
+                //而且default也要改成像下面的這樣defaults這樣(下面這兩行是一組的)
+                url: "{action}/{controller}/{id}",
+                //defaults的語句中，action = xxx和index = ooo位置會決定預設的網址列action和controller的位置
+                //寫成像下面這樣，預設的網址列就會是http://localhost:4020/index/Home
+                defaults: new { action = "Index", controller = "Home", id = UrlParameter.Optional }
             );
         }
     }
