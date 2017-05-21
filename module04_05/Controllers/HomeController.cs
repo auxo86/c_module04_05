f80524d9673e3f1deae97981faa1a20f108b620a
van chung chen <auxo_86@hotmail.com>
2017-05-21 11:13:20 +0800

    加入專案檔案。

---
+++ module04_05/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace module04_05.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET: Home/GetVideo
+        //利用action取檔案
+        public ActionResult GetVideo()
+        {
+            return File("~/Content/movie.mp4", "video/mp4");
+        }
+    }
+}
